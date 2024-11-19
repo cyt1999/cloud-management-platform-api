0043b445f2875967afed225ce4b05fff501a1ae4
chenyutong <xb990815@gmail.com>
2024-11-19 15:00:21 +0800

    新增客户信息字段，避免查询不必要的敏感信息

--- CustomerApp/models.py
+++ CustomerApp/models.py
@@ -6,7 +6,8 @@ class Customer(models.Model):
     cloud_platform_type = models.CharField(max_length=100)  # 云平台类型
     account = models.CharField(max_length=100)  # 账号
     password = models.CharField(max_length=100)  # 密码
-    key = models.CharField(max_length=100)  # 密钥
+    access_key_id = models.CharField(max_length=100)  # AccessKey ID
+    access_key_secret = models.CharField(max_length=100)  # AccessKey Secret
 
     def __str__(self):
         return self.name  # 返回客户名称作为对象的字符串表示 
--- CustomerApp/serializers.py
+++ CustomerApp/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Customer
 
-class CustomerSerializer(serializers.ModelSerializer):
+class CustomerCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
-        fields = '__all__' 
+        fields ='__all__'  
+
+class CustomerRetrieveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields =   ['id','name','cloud_platform_type','account','password']# 查询时不返回 AccessKey ID 和 AccessKey Secret
